01e88de91d793e711a6f2a373c9fa3c1c4464622
lizanle <491823689@qq.com>
2018-09-21 17:24:12 +0800

    类名错误

--- Misc/PalindromicPrime.java
+++ Misc/PalindromicPrime.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PalindromePrime {
+public class PalindromicPrime {
 
     public static void main(String[] args) { // Main funtion
         Scanner in = new Scanner(System.in);
